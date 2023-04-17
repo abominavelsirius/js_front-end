698adfb80da573c56e3020ae30448827511bea4b
Abominavel_Sirius <abominavelsirius@gmail.com>
2023-04-17 17:25:10 -0300

    multiplos itens no localStorage

--- armazenando dados no navegador/js/3.explicando.js
+++ armazenando dados no navegador/js/3.explicando.js
@@ -31,16 +31,24 @@ form.addEventListener('submit', (evento) => {
 
         lista.appendChild(novoItem) 
 
-        
+
+        // no JS, toda vez que temos uma chave valor e a gente quer salvar o que se chaam de dicionário, usamos objeto
+        // então vamos transformar esse elemento chamado itemAtual em um objeto
+
         const itemAtual = {
             "nome": nome,
             "quantidade": quantidade
         }
 
-        itens.push(itemAtual) 
+        itens.push(itemAtual) // a função para inserir o elemento no array 
 
+
+        // agora ao invés de enviarmos duas informações, vamos usar uma unica informação chamada "itens"
         localStorage.setItem("item", JSON.stringify(itens)) 
 
+        // só que ele salva como object, o localStorage só lê elemento do tipo JSON, só lê uma string
+        // então é preciso transformar o elemento em string, colocando JSON.stringfly (transformará o JSon em string)
+        // ele salva com as chaves, como a representação de um objeto
 
     }
 
